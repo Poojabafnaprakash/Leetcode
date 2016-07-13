a89c71e1e6cba2453e7c871d6a93779e1d383459
Poojabafnaprakash <poojabafnaprakash@gmail.com>
2016-07-13 15:53:49 -0700

    Reversed string optimal

--- ReverseString.cc
+++ ReverseString.cc
@@ -3,6 +3,28 @@ public:
     string reverseString(string s) {
         char temp;
         int length = s.length();
+        for(int i=0;i<=(length/2);i++)
+        {
+        temp = s[length-i-1];
+        s[length-i-1] = s[i];
+        s[i] = temp;
+        }
+        return s;
+    }
+};
+
+
+
+
+/*
+ 
+ //Alternate solution
+ 
+ class Solution {
+public:
+    string reverseString(string s) {
+        char temp;
+        int length = s.length();
         int last = length;
         int first = 0;
         for(int i=(length-1);i>=length/2;i--)
@@ -16,3 +38,5 @@ public:
         return s;
     }
 };
+
+*/
